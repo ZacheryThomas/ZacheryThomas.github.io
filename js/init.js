9d7d7ae17d2187e1503654264b5eeaa5b932270e
Zachery Thomas <zacthoma@cisco.com>
2017-07-08 20:31:40 -0400

    Added quotes

--- js/init.js
+++ js/init.js
@@ -47,8 +47,7 @@
 		   var active_section;
 
 			active_section = $(this);
-            console.log(active_section)
-            
+
 			if (direction === "up") active_section = active_section.prev();
 
 			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
