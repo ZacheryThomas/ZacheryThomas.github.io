75f6591aee28f5f917bffe691b842009adf39a31
Zach Thomas <zacherythomas12@gmail.com>
2017-08-15 17:33:44 -0400

    Added new tests for videochat

---
+++ rtc_tests/index.js
@@ -0,0 +1,16 @@
+var webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remoteVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true,
+
+    url: 'http://ec2-34-207-147-139.compute-1.amazonaws.com:8888'
+});
+
+// we have to wait until it's ready
+webrtc.on('readyToCall', function () {
+    // you can name it anything
+    webrtc.joinRoom('your awesome room name');
+});
