a91ed353e4e0e7ef20b4f5a819ef3f7acbef9d09
Zach Thomas <zacherythomas12@gmail.com>
2017-10-06 00:26:43 -0400

    Updated style

--- chat/speech.js
+++ chat/speech.js
@@ -5,21 +5,18 @@ This handles STT for sending data as well as TTS for recieving data
 // If you are using VanillaJS
 const artyom = new Artyom();
 
-var UserDictation = undefined
 
-// Speech Recognition setup
-function start_speech_rec_send(conn) {
-    recognizing = true
-
+// Conneciton Established
+function speech_events(conn) {
     let lastLine = ''
-    UserDictation = artyom.newDictation({
+    let UserDictation = artyom.newDictation({
         continuous: true, // Enable continuous if HTTPS connection
         onResult: function (text) {
-        if (text == '') {
-            console.log('you said: ' + lastLine);
-            con.send(lastLine)
-        }
-        lastLine = text
+            if (text == '') {
+                console.log('you said: ' + lastLine);
+                conn.send(lastLine)
+            }
+            lastLine = text
         },
         onStart: function () {
             console.log("Dictation started by the user");
@@ -29,20 +26,8 @@ function start_speech_rec_send(conn) {
         }
     });
 
-    UserDictation.start();
-}
-
-
-function stop_speech_rec_send() {
-    recognizing = false
-    UserDictation.stop()
-}
-
-
-// Conneciton Established
-function speech_events(conn) {
     conn.on('open', function () {
-        start_speech_rec_send(conn)
+        UserDictation.start()
 
         conn.on('data', function (data) {
             console.log("what the other person said: " + data)
@@ -51,6 +36,6 @@ function speech_events(conn) {
     });
 
     conn.on('close', function () {
-        stop_speech_rec_send()
+        UserDictation.end()
     });
 }
