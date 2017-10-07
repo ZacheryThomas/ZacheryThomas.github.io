6cf8edc8d87b6ac821fddb588779fe12089f5e29
Zach Thomas <zacherythomas12@gmail.com>
2017-10-07 15:28:05 -0400

    Changed scope of speech

--- chat/speech.js
+++ chat/speech.js
@@ -9,38 +9,38 @@ let listening = false
 
 // Conneciton Established
 function speech_events(conn) {
-    let lastLine = ''
-    let UserDictation = artyom.newDictation({
-        continuous: true, // Enable continuous if HTTPS connection
-        onResult: function (text) {
-            if (text == '') {
-                console.log('you said: ' + lastLine);
-                conn.send(lastLine)
-            }
-            lastLine = text
-        },
-        onStart: function () {
-            console.log("Dictation started by the user");
-        },
-        onEnd: function () {
-            alert("Dictation stopped by the user");
-        }
-    });
-    
     if (!listening){
-        UserDictation.start()
         listening = true
-    }
-
-    conn.on('open', function () {
-        conn.on('data', function (data) {
-            console.log("what the other person said: " + data)
-            responsiveVoice.speak("" + data)
+        let lastLine = ''
+        let UserDictation = artyom.newDictation({
+            continuous: true, // Enable continuous if HTTPS connection
+            onResult: function (text) {
+                if (text == '') {
+                    console.log('you said: ' + lastLine);
+                    conn.send(lastLine)
+                }
+                lastLine = text
+            },
+            onStart: function () {
+                console.log("Dictation started by the user");
+            },
+            onEnd: function () {
+                alert("Dictation stopped by the user");
+            }
         });
-    });
 
-    conn.on('close', function () {
-        UserDictation.stop()
-        listening = false
-    });
+        UserDictation.start()
+        
+        conn.on('open', function () {
+            conn.on('data', function (data) {
+                console.log("what the other person said: " + data)
+                responsiveVoice.speak("" + data)
+            });
+        });
+
+        conn.on('close', function () {
+            UserDictation.stop()
+            listening = false
+        });
+    }
 }
